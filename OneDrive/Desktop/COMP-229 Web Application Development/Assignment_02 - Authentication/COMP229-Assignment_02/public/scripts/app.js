f9b627845a2270ad3dc223dff7dcbbfca6203263
cymdiesel <cymantha.diesel@gmail.com>
2022-10-21 13:14:47 -0600

    Updated content on all pages of portfolio

---
+++ OneDrive/Desktop/COMP-229 Web Application Development/Assignment_02 - Authentication/COMP229-Assignment_02/public/scripts/app.js
@@ -0,0 +1,12 @@
+ /* COMP-229 Assignment 02
+    Cymantha Diesel
+    301226808
+    October 21, 2022 */
+
+(function(){
+    function Start(){
+        console.log("App Started!");
+    }
+
+    window.addEventListener('load', Start);
+})();
